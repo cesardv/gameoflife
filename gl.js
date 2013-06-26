9dda35af55d0f7745f339c26dde80278c08a07d6
Huan Minh Vu <hectoronevu@gmail.com>
2013-06-26 14:38:12 -0400

    fixing mergeconflicts

--- gl.js
+++ gl.js
@@ -4,18 +4,24 @@
     var time;
     // pop is current population
     var pop = 0;
-    //popm string for population
-    var popm = "Population: " + pop;
+    // gen is current generation
+    var gen = 0;
+    //popm string for population and generation
+    var popm = "Population: " + pop + "; Generation: " + gen;
 function createTable() {
+    // rl is row length, cl is column length
+    var rl = document.forms.game.row.value;
+    var cl = document.forms.game.col.value;
     pop = 0;
+    gen = 0;
     document.getElementById("container").innerHTML = "";
     // t is current array
-    t = new Array(10);
-    for (var i = 0; i < 10; i++) {
-        t[i] = new Array(10);
+    t = new Array(rl);
+    for (var i = 0; i < rl; i++) {
+        t[i] = new Array(cl);
     }
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
+    for (var i = 0; i < rl; i++) {
+        for (var j = 0; j < cl; j++) {
             t[i][j] = Math.floor(Math.random() * 2);
         }
     }
@@ -25,10 +31,10 @@ function createTable() {
     baseCell.className = "squarecell";
     var container = document.getElementById('container');
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < rl; i++) {
         //Create a new row
         var myRow = baseRow.cloneNode(false);
-        for (var j = 0; j < 10; j++) {
+        for (var j = 0; j < cl; j++) {
             //Create a new cell, you could loop this for multiple cells
             var myCell = baseCell.cloneNode(false);
             myCell.innerHTML = t[i][j];
@@ -50,9 +56,9 @@ function createTable() {
         //Append new row
         table.appendChild(myRow);
     }
-
+    gen++;
     container.appendChild(table);
-    popm = "Population: " + pop;
+    popm = "Population: " + pop + "; Generation: " + gen;
     document.forms.game.lives.value = popm;
 }
 function start() {
@@ -66,22 +72,27 @@ function stop(){
 }
 function reset() {
     createTable();
+    gen = 0;
 }
 function next(){
+
+    var rl = document.forms.game.row.value;
+    var cl = document.forms.game.col.value;
     pop = 0;
+    gen++;
     document.getElementById("container").innerHTML = "";
     var alive_count;
-    var n = new Array(10);
-    for (var i = 0; i < 10; i++) {
-        n[i] = new Array(10);
+    var n = new Array(rl);
+    for (var i = 0; i < rl; i++) {
+        n[i] = new Array(cl);
     }
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
+    for (var i = 0; i < rl; i++) {
+        for (var j = 0; j < cl; j++) {
             n[i][j] = 0;
         }
     }
-    for(var i = 0; i < 10; i++){
-        for(var j = 0; j < 10; j++){
+    for(var i = 0; i < rl; i++){
+        for(var j = 0; j < cl; j++){
             alive_count = 0;
             // left top alive
             if(i-1 >= 0 && j - 1>=0)
@@ -90,22 +101,23 @@ function next(){
             if(i - 1 >= 0)
                 if(t[i-1][j] == 1) alive_count++;
             // right top alive
-            if(i-1 >= 0 && j + 1 <= 9)
+
+            if(i-1 >= 0 && j + 1 <= (cl-1))
                 if(t[i-1][j+1] == 1) alive_count++;
             // left alive
             if(j-1>=0)
                 if(t[i][j-1]==1) alive_count++;
             // right alive
-             if(j+1<=9)
+             if(j+1<=(cl-1))
                 if(t[i][j+1]==1) alive_count++;
             // bottom left alive
-            if(i+1 <= 9 && j - 1>=0)
+            if(i+1 <= (rl-1) && j - 1>=0)
                 if(t[i+1][j-1] == 1) alive_count++;
             // bottom alive
-            if(i + 1 <= 9)
+            if(i + 1 <= (rl-1))
                 if(t[i+1][j] == 1) alive_count++;
             // bottom right alive
-            if(i+1 <= 9 && j + 1<=9)
+            if(i+1 <= (rl-1) && j + 1<=(cl-1))
                 if(t[i+1][j+1] == 1) alive_count++;
             // set up new array
             if(t[i][j] == 1 && (alive_count < 2 || alive_count > 3))
@@ -127,10 +139,10 @@ function next(){
     baseCell.className = "squarecell";
     var container = document.getElementById('container');
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < rl; i++) {
         //Create a new row
         var myRow = baseRow.cloneNode(false);
-        for (var j = 0; j < 10; j++) {
+        for (var j = 0; j < cl; j++) {
             //Create a new cell, you could loop this for multiple cells
             var myCell = baseCell.cloneNode(false);
             myCell.innerHTML = n[i][j];
@@ -153,11 +165,14 @@ function next(){
     }
 
     container.appendChild(table);
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
+    for (var i = 0; i < rl; i++) {
+        for (var j = 0; j < cl; j++) {
             t[i][j] = n[i][j];
         }
     }
-    popm = "Population: " + pop;
+    popm = "Population: " + pop + "; Generation: " + gen;
     document.forms.game.lives.value = popm;
+}
+function change(){
+
 }
